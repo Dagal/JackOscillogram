2d9276f641dbeec45aa2a364490a09df167f3319
Dagal <dejarding@gmail.com>
2014-12-22 21:57:41 +0100

    Premier post fonctionnel sur un seul canal.

---
+++ JackOscillogram.pro
@@ -0,0 +1,19 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2014-12-16T17:39:37
+#
+#-------------------------------------------------
+
+QT       += core gui
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = JackOscillogram
+TEMPLATE = app
+
+LIBS += -ljack
+
+SOURCES += main.cpp\
+				MainWindow.cpp
+
+HEADERS  += MainWindow.h
---
+++ MainWindow.h
@@ -0,0 +1,40 @@
+﻿#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QPainter>
+#include <QTimer>
+#include <QDebug>
+
+#include <jack/jack.h>
+
+class MainWindow : public QMainWindow
+{
+	Q_OBJECT
+
+public:
+	MainWindow(QWidget *parent = 0);
+	~MainWindow();
+
+	static int staticProcess(jack_nframes_t nframes, void* arg);
+	int process(jack_nframes_t nframes);
+	static int staticFrameRateChanged(jack_nframes_t nframes, void* arg);
+	int frameRateChanged(jack_nframes_t nframes);
+	static int staticBufferSizeChanged(jack_nframes_t nframes, void* arg);
+	int bufferSizeChanged(jack_nframes_t nframes);
+	void paintEvent(QPaintEvent* ev);
+
+private slots:
+	void startingBlock();
+
+private:
+	QTimer mTimer1;
+	jack_client_t* mClientJack;
+	jack_port_t* mPortJack;
+	jack_default_audio_sample_t* mBufferJack;
+	QVector<float> mBufferDatagram;
+	jack_nframes_t mJackFrames;
+	jack_nframes_t mJackBufferSize;
+};
+
+#endif // MAINWINDOW_H
